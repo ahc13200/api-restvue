db5ee26a96e189c5b65adfac12e38551474f40b6
Amanda <ahc13200@gmail.com>
2024-12-21 13:52:46 -0500

    websocket ajustes

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,9 +40,9 @@ return [
 //                'useTLS' => true,
                 'verify' => false,
                 'encrypted' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http',
+                'host' => env('PUSHER_HOST'),
+                'port' => env('PUSHER_PORT'),
+                'scheme' => env('PUSHER_SCHEME'),
             ],
         ],
 
