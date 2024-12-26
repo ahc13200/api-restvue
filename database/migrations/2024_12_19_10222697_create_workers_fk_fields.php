3b9332ddd9a2f30129accb8e5b911eee81e0ed02
Amanda <ahc13200@gmail.com>
2024-12-26 14:26:18 -0500

    ajustes

--- database/migrations/2024_12_19_10222697_create_workers_fk_fields.php
+++ database/migrations/2024_12_19_10222697_create_workers_fk_fields.php
@@ -5,9 +5,8 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 /**
-   Foreign Keys Workers table */
-return new class extends Migration
-{
+ * Foreign Keys Workers table */
+return new class extends Migration {
 
     /**
      * Run the migrations.
@@ -20,17 +19,17 @@ return new class extends Migration
         $connection = Schema::connection('db');
         $exist_table = $connection->hasTable('admin.workers');
         if ($exist_table) {
-            $foreignkey_list=Schema::getForeignKeys('admin.workers');
-            $find=array_search('admin_workers_user_id_foreign', array_column(json_decode(json_encode($foreignkey_list),TRUE), 'name'));
+            $foreignkey_list = Schema::getForeignKeys('admin.workers');
+            $find = array_search('admin_workers_user_id_foreign', array_column(json_decode(json_encode($foreignkey_list), TRUE), 'name'));
             if (!Schema::hasIndex('admin.workers', 'admin_workers_user_id_foreign') && !is_numeric($find))
                 Schema::table('admin.workers', function (Blueprint $table) {
-                    $table->foreign('user_id')->references('id')->on('security.users')->onDelete('restrict');
+                    $table->foreign('user_id')->references('id')->on('security.users')->onDelete('cascade');
                 });
-           };
+        };
 
     }
 
-   public function down()
+    public function down()
     {
         Schema::dropIfExists('admin.workers');
 
--- database/migrations/2024_12_19_10222711_create_worker_area_turns_fk_fields.php
+++ database/migrations/2024_12_19_10222711_create_worker_area_turns_fk_fields.php
@@ -5,9 +5,8 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 /**
-   Foreign Keys Worker_area_turns table */
-return new class extends Migration
-{
+ * Foreign Keys Worker_area_turns table */
+return new class extends Migration {
 
     /**
      * Run the migrations.
@@ -20,29 +19,29 @@ return new class extends Migration
         $connection = Schema::connection('db');
         $exist_table = $connection->hasTable('admin.worker_area_turns');
         if ($exist_table) {
-            $foreignkey_list=Schema::getForeignKeys('admin.worker_area_turns');
-            $find=array_search('admin_worker_area_turns_area_id_foreign', array_column(json_decode(json_encode($foreignkey_list),TRUE), 'name'));
+            $foreignkey_list = Schema::getForeignKeys('admin.worker_area_turns');
+            $find = array_search('admin_worker_area_turns_area_id_foreign', array_column(json_decode(json_encode($foreignkey_list), TRUE), 'name'));
             if (!Schema::hasIndex('admin.worker_area_turns', 'admin_worker_area_turns_area_id_foreign') && !is_numeric($find))
                 Schema::table('admin.worker_area_turns', function (Blueprint $table) {
                     $table->foreign('area_id')->references('id')->on('admin.areas')->onDelete('restrict');
                 });
-            $foreignkey_list=Schema::getForeignKeys('admin.worker_area_turns');
-            $find=array_search('admin_worker_area_turns_turn_id_foreign', array_column(json_decode(json_encode($foreignkey_list),TRUE), 'name'));
+            $foreignkey_list = Schema::getForeignKeys('admin.worker_area_turns');
+            $find = array_search('admin_worker_area_turns_turn_id_foreign', array_column(json_decode(json_encode($foreignkey_list), TRUE), 'name'));
             if (!Schema::hasIndex('admin.worker_area_turns', 'admin_worker_area_turns_turn_id_foreign') && !is_numeric($find))
                 Schema::table('admin.worker_area_turns', function (Blueprint $table) {
                     $table->foreign('turn_id')->references('id')->on('admin.turns')->onDelete('restrict');
                 });
-            $foreignkey_list=Schema::getForeignKeys('admin.worker_area_turns');
-            $find=array_search('admin_worker_area_turns_worker_id_foreign', array_column(json_decode(json_encode($foreignkey_list),TRUE), 'name'));
+            $foreignkey_list = Schema::getForeignKeys('admin.worker_area_turns');
+            $find = array_search('admin_worker_area_turns_worker_id_foreign', array_column(json_decode(json_encode($foreignkey_list), TRUE), 'name'));
             if (!Schema::hasIndex('admin.worker_area_turns', 'admin_worker_area_turns_worker_id_foreign') && !is_numeric($find))
                 Schema::table('admin.worker_area_turns', function (Blueprint $table) {
-                    $table->foreign('worker_id')->references('id')->on('admin.workers')->onDelete('restrict');
+                    $table->foreign('worker_id')->references('id')->on('admin.workers')->onDelete('cascade');
                 });
-           };
+        };
 
     }
 
-   public function down()
+    public function down()
     {
         Schema::dropIfExists('admin.worker_area_turns');
 
